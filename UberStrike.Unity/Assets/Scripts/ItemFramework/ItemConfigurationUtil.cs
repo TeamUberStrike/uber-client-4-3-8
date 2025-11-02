a8ac240ab0886479c657634d17857c2173eec53c
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-11-02 12:22:13 +0100

    debugging functional item exception

--- UberStrike.Unity/Assets/Scripts/ItemFramework/ItemConfigurationUtil.cs
+++ UberStrike.Unity/Assets/Scripts/ItemFramework/ItemConfigurationUtil.cs
@@ -12,17 +12,30 @@ public static class ItemConfigurationUtil
     {
         CloneUtil.CopyAllFields(config, item);
 
+        //CmuneDebug.LogError("Before CopyProperties for loop");
         //customize fields
+        if (item.ID == 1234)
+        {
+            CmuneDebug.LogError("CopyProperties called for item ID 1234");
+        }
+        else
+        {
+            CmuneDebug.LogError("CopyProperties called for item ID " + item.ID);
+        }
         foreach (var p in ReflectionHelper.GetAllFields(config.GetType(), true))
         {
+            //CmuneDebug.LogError("Before GetCustomPropertyName");
             string name = GetCustomPropertyName(p);
+            //CmuneDebug.LogError("Custom property name: " + name);
 
             if (!string.IsNullOrEmpty(name) && item.CustomProperties != null && item.CustomProperties.ContainsKey(name))
             {
+                CmuneDebug.LogError("inside !IsNullOrEmpty ");
                 //configure using custom property
                 p.SetValue(config, Convert(item.CustomProperties[name], p.FieldType));
             }
         }
+        //CmuneDebug.LogError("After CopyProperties for loop");
     }
 
     private static string GetCustomPropertyName(FieldInfo info)
